54466be2b70b493de69fb7ec701a6494031f8e2e
Lachezar Georgiev <49004684+glachezar@users.noreply.github.com>
2024-05-26 17:43:54 +0300

    Added charts in test component

--- Pages/TestComponent.razor.cs
+++ Pages/TestComponent.razor.cs
@@ -15,36 +15,4 @@ public partial class TestComponent
     private async Task LoadProducts() =>
         productList = (await ProductService.GetAllProductsAsync()).ToList();
 
-    private async Task OnRowInserting(Product product)
-    {
-        await ProductService.AddProductAsync(product);
-        await LoadProducts();
-    }
-
-    private async Task OnRowUpdating(GridEditModelSavingEventArgs p)
-    {
-        if (p.IsNew)
-        {
-            var product = (Product)p.EditModel;
-            product.DateAdded = DateTimeValue;
-            await OnRowInserting(product);
-        }
-        else
-        {
-            var product = (Product)p.EditModel;
-            //The line below can be uncommented if you want to be able to redact the DateAdded field
-            //product.DateAdded = DateTimeValue;
-            await ProductService.UpdateProductAsync(product);
-        }
-        await LoadProducts();
-    }
-
-    private async Task OnRowDeleting(GridDataItemDeletingEventArgs e)
-    {
-        await ProductService.DeleteProductAsync((e.DataItem as Product).Id);
-        await LoadProducts();
-    }
-
-    void OnDateChanged(DateTime newValue) =>
-        DateTimeValue = newValue;
 }
