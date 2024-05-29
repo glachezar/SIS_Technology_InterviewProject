b264645caf14f13c2550a85edada0e26fa56365b
glachezar <49004684+glachezar@users.noreply.github.com>
2024-05-29 15:30:12 +0300

    Added Domain, Application and Infrastructure for scalability

---
+++ Domain/Entities/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain;
+
+public class Product
+{
+    public int Id { get; set; }
+
+    public string ProductName { get; set; } = null!;
+
+    public string Category { get; set; } = String.Empty;
+
+    public decimal UnitPrice { get; set; }
+
+    public DateTime DateAdded { get; set; }
+}
+
---
+++ Infrastructure/Dapper/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System.Data;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace Infrastructure.Dapper;
+
+public class ApplicationDbContext
+{
+    readonly IConfiguration _configuration;
+    readonly string _connectionString;
+
+    public ApplicationDbContext(IConfiguration configuration)
+    {
+        _configuration = configuration;
+        _connectionString = _configuration.GetConnectionString("DefaultConnection");
+    }
+
+    public IDbConnection CreateConnection()
+    {
+        return new SqlConnection(_connectionString);
+    }
+}
