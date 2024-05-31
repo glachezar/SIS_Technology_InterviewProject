e6832934f5e564a42aa54de56bf0559fb4bd81ba
Lachezar Georgiev <49004684+glachezar@users.noreply.github.com>
2024-05-31 13:35:16 +0300

    Added dependency injection setup and updated DI class
    A new package reference to `Microsoft.Extensions.DependencyInjection.Abstractions` version `8.0.1` has been added in `Application.csproj` for dependency injection abstractions. In `DI.cs`, the namespace declaration has been relocated, and new using directives have been added for the dependency injection setup. The `DI` class has been modified to a `public static` class, and a new method `AddApplication` has been added for setting up the dependency injection for `IProductService`.

--- Application/DI.cs
+++ Application/DI.cs
@@ -1,5 +1,15 @@
-﻿namespace Application;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Application.Interfaces;
+using Application.Services;
 
-internal class DI
+namespace Application;
+
+public static class DI
 {
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddScoped<IProductService, ProductService>();
+
+        return services;
+    }
 }
