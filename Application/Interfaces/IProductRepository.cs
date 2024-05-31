3701e46191d658f5f5de5f218c356da54667b797
Lachezar Georgiev <49004684+glachezar@users.noreply.github.com>
2024-05-31 13:27:22 +0300

    Added Product repository

---
+++ Application/Interfaces/IProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities;
+
+namespace Application.Interfaces;
+
+internal interface IProductRepository
+{
+    // Create
+    public Task<int> AddProductAsync(Product product);
+
+    // Read
+    public Task<Product> GetProductByIdAsync(int id);
+
+    public Task<IEnumerable<Product>> GetAllProductsAsync();
+
+    // Update
+    public Task<int> UpdateProductAsync(Product product);
+
+    // Delete
+    public Task<int> DeleteProductAsync(int id);
+}
