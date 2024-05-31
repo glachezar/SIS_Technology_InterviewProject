7d0b095b2fae46278bcb86ec5d634bf5e7b98949
Lachezar Georgiev <49004684+glachezar@users.noreply.github.com>
2024-05-31 14:40:35 +0300

    Added DbContext class and updated package references
    Added a new `DbContext.cs` file to the `Infrastructure.Dapper.Repositories` namespace. This class is responsible for creating a database connection using the "DefaultConnection" string from the application's configuration. If the connection string is not found, it throws an `InvalidOperationException`. The `DbContext` class uses the `Microsoft.Data.SqlClient` package to create an `IDbConnection` instance.
    
    Updated the `Infrastructure.csproj` file to include new package references: `Dapper` version 2.1.35, `Microsoft.Data.SqlClient` version 5.2.0, and `Microsoft.Extensions.Configuration.Abstractions` version 8.0.0. These packages are necessary for the `DbContext` class to function correctly.

---
+++ Infrastructure/Dapper/Repositories/DbContext.cs
@@ -0,0 +1,23 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace Infrastructure.Dapper.Repositories;
+
+public class DbContext
+{
+    readonly IConfiguration _configuration;
+    readonly string _connectionString;
+
+    public DbContext(IConfiguration configuration)
+    {
+        _configuration = configuration;
+        _connectionString = _configuration.GetConnectionString("DefaultConnection") ??
+                                 throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+    }
+
+    public IDbConnection CreateConnection()
+    {
+        return new SqlConnection(_connectionString);
+    }
+}
