7da0e0d77202f5d858cf651ce614dc18fea01414
Lachezar Georgiev <49004684+glachezar@users.noreply.github.com>
2024-05-31 14:45:28 +0300

    Added ProductRepository class and updated Infrastructure.csproj
    Added a new `ProductRepository` class in the `Infrastructure.Dapper.Repositories` namespace. This class implements the `IProductRepository` interface and provides CRUD operations on `Product` entities using Dapper and `DbContext`. The methods include `AddProductAsync`, `GetProductByIdAsync`, `GetAllProductsAsync`, `UpdateProductAsync`, and `DeleteProductAsync`. Also, removed the `<Folder Include="Dapper\Repositories\" />` line from the `Infrastructure.csproj` file, indicating a change in the file structure.

---
+++ Infrastructure/Dapper/Repositories/ProductRepository.cs
@@ -0,0 +1,62 @@
+﻿using Application.Interfaces;
+using Dapper;
+using Domain.Entities;
+using static Infrastructure.Dapper.SqlConstants;
+
+namespace Infrastructure.Dapper.Repositories;
+
+public class ProductRepository : IProductRepository
+{
+    readonly DbContext _context;
+
+    public ProductRepository(DbContext context)
+    {
+        _context = context;
+    }
+
+    //Create
+    public async Task<int> AddProductAsync(Product product)
+    {
+        using (var connection = _context.CreateConnection())
+        {
+            return await connection.QuerySingleAsync<int>(ProductSql.CreateProduct, product);
+        }
+    }
+
+    // Read
+    public async Task<Product> GetProductByIdAsync(int id)
+    {
+        using (var connection = _context.CreateConnection())
+        {
+            return await connection.QuerySingleOrDefaultAsync<Product>(ProductSql.GetProductById, new { Id = id }) ??
+                throw new KeyNotFoundException($"Product with id: {id} not found!");
+        }
+    }
+
+    public async Task<IEnumerable<Product>> GetAllProductsAsync()
+    {
+        using (var connection = _context.CreateConnection())
+        {
+            var products = await connection.QueryAsync<Product>(ProductSql.GetAllProducts);
+            return products.ToList();
+        }
+    }
+
+    // Update
+    public async Task<int> UpdateProductAsync(Product product)
+    {
+        using (var connection = _context.CreateConnection())
+        {
+            return await connection.ExecuteAsync(ProductSql.UpdateProduct, product);
+        }
+    }
+
+    // Delete
+    public async Task<int> DeleteProductAsync(int id)
+    {
+        using (var connection = _context.CreateConnection())
+        {
+            return await connection.ExecuteAsync(ProductSql.DeleteProduct, new { Id = id });
+        }
+    }
+}
