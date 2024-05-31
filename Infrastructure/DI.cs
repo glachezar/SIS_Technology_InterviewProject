e53c90aebee5e60e7a6496c5964be060bfa80660
Lachezar Georgiev <49004684+glachezar@users.noreply.github.com>
2024-05-31 14:47:14 +0300

    Subject: Added DI class and configured dependency injection
    Body:
    Added new namespaces to the `DI.cs` file for dependency injection, application interfaces, and Dapper infrastructure. Created a new static `DI` class in the `Infrastructure` namespace to configure dependency injection. Introduced an extension method `AddInfrastructure` in the `DI` class to extend the `IServiceCollection` interface for service registration. The `AddInfrastructure` method registers `DbContext` and `ProductRepository` classes with a scoped lifetime. `DbContext` is registered without an interface, while `ProductRepository` is registered against the `IProductRepository` interface. The method returns the modified service collection, enabling method chaining on the `IServiceCollection`.

---
+++ Infrastructure/DI.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Application.Interfaces;
+using Infrastructure.Dapper;
+using Infrastructure.Dapper.Repositories;
+
+namespace Infrastructure;
+
+public static class DI
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        services.AddScoped<DbContext>();
+        services.AddScoped(typeof(IProductRepository), typeof(ProductRepository));
+
+        return services;
+    }
+}
