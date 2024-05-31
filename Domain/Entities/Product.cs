b8b0f7664248c1f495e950fc80466f898890f8ae
Lachezar Georgiev <49004684+glachezar@users.noreply.github.com>
2024-05-31 13:24:38 +0300

    Added entity Product in Domain layer

---
+++ Domain/Entities/Product.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities;
+
+[Table("Products")]
+public class Product
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    [Column("ProductName", TypeName = "nvarchar(90)")]
+    public string ProductName { get; set; } = null!;
+
+    [Column("Category", TypeName = "nvarchar(90)")]
+    public string Category { get; set; } = String.Empty;
+
+    [Column("UnitPrice")]
+    public decimal UnitPrice { get; set; }
+
+    [Column("DateAdded")]
+    public DateTime DateAdded { get; set; }
+}
